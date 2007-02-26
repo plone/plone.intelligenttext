2249072935d5dc9737a060af997f39b74fa68dd5
Hanno Schlichting <hanno@hannosch.eu>
2007-02-26 08:02:00 +0000

    Updated setup.py with dependency and framework classifier.
    svn path=/plone.intelligenttext/trunk/; revision=13248

--- setup.py
+++ setup.py
@@ -10,6 +10,7 @@ setup(name='plone.intelligenttext',
 """,
       # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
+        "Framework :: Plone",
         "Framework :: Zope2",
         "Framework :: Zope3",
         "Programming Language :: Python",
@@ -25,7 +26,7 @@ setup(name='plone.intelligenttext',
       include_package_data=True,
       zip_safe=False,
       install_requires=[
-          # -*- Extra requirements: -*-
+        'setuptools',
       ],
       entry_points="""
       # -*- Entry points: -*-
